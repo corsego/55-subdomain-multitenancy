d50cf25f7bfbde6a7a6d3db934e7b9a6a64a7c33
Yaro <yshmarov@gmail.com>
2021-09-24 12:38:43 +0000

    account has many posts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ApplicationRecord
   validates :name, :subdomain, presence: true
   validates :subdomain, uniqueness: true
+
+  has_many :posts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_123724) do
+ActiveRecord::Schema.define(version: 2021_09_24_123740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2021_09_24_123724) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "account_id", null: false
+    t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
+  add_foreign_key "posts", "accounts"
 end
