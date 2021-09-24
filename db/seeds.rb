74da6ca2ea6a1155cd04846775d4865ff4923dc8
Yaro <yshmarov@gmail.com>
2021-09-24 12:32:19 +0000

    seed accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+accounts = %w(Good Better Best)
+
+puts "I'm creating your accounts "
+accounts.each do |name|
+  Account.create!(
+    name: name,
+    subdomain: name.downcase
+  )
+end
+puts "Done creating your accounts"
